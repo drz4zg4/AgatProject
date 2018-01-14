c398b1da449d83aca75a6cf66753282ca0d73254
drz4zg4 <radoslaw.iwaszko@gmail.com>
2018-01-14 13:49:45 +0100

    Modyfikacja szablonu, by pokazywal posty z bazy danych

--- AgatProject/agat/views.py
+++ AgatProject/agat/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-
-
+from django.utils import timezone
+from .models import FlatPanelDispla
 
 def index(request):
-    return render(request, 'agat/index.html', {})
+    posts = FlatPanelDispla.objects.filter(data_testu__lte=timezone.now()).order_by('published_date')
+    return render(request, 'agat/index.html', {'posts': posts})
